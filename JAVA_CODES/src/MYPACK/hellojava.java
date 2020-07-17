79be6be20abbdab13306eb8a86cd51acf365ca5b
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2020-07-17 17:01:03 +0530

    first commit

---
+++ JAVA_CODES/src/MYPACK/hellojava.java
@@ -0,0 +1,10 @@
+package MYPACK;
+
+public class hellojava {
+
+	public static void main(String[] args) {
+		System.out.println("hello vanshilal laddoo bhaiya");// TODO Auto-generated method stub
+
+	}
+
+}
