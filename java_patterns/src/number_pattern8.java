467937f4022bf374bd92077d7ce55f766b2f60e7
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2020-09-04 14:57:40 +0530

    pattern

---
+++ java_patterns/src/number_pattern8.java
@@ -0,0 +1,20 @@
+/*
+11111
+2222
+333
+44
+5
+*/
+public class number_pattern8 {
+    public static void main(String[] args)
+    {
+        for(int i=1;i<6;i++)
+        {
+            for(int j=5;j>=i;j--)
+            {
+                System.out.print(i);
+            }
+            System.out.println(" ");
+        }
+    }
+}
