c996dafee569313e2c246ff6309ecd6fa9dc4c57
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2020-09-04 14:57:00 +0530

    pattern

---
+++ java_patterns/src/number_pattern4.java
@@ -0,0 +1,21 @@
+/*
+55555
+44444
+33333
+22222
+11111
+*/
+
+public class number_pattern4 {
+    public static void main(String[] args)
+    {
+        for(int i=5;i>0;i--)
+        {
+            for(int j=1;j<6;j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println(" ");
+        }
+    }
+}
