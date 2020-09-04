1753b76baac96855596d695bd6e7e2d9dba05be3
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2020-09-04 16:37:26 +0530

    string method

---
+++ javastring/src/endswidth_method.java
@@ -0,0 +1,7 @@
+//The endsWidth() method checks whether a string ends with the specified character(s).
+public class endswidth_method {
+    public static void main(String[] args){
+        String str="sanidhya";
+        System.out.println(str.endsWith("n"));
+    }
+}
