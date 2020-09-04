ea39e784e08897cbb13deb978de4991d27edf17f
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2020-09-04 14:57:54 +0530

    pattern

---
+++ java_patterns/src/number_pattern10.java
@@ -0,0 +1,21 @@
+/*
+55555
+4444
+333
+22
+1
+*/
+
+public class number_pattern10 {
+    public static void main(String[] args)
+    {
+        for(int i=5;i>0;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println(" ");
+        }
+    }
+}
