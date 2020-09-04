91098ff41b295efd08e3f472267e60e87e08996d
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2020-09-04 16:37:36 +0530

    string method

---
+++ javastring/src/equals_method.java
@@ -0,0 +1,8 @@
+// compares two string and returns true if the strings are equal,and false if not.
+public class equals_method {
+    public static void main(String[] args){
+        String str1="hello";
+        String str2="HELLO";
+        System.out.println(str1.equals(str2));
+    }
+}
