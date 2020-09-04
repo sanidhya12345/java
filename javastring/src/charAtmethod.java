1c607c689ff1e80e8ad7897541485da1d0ac8dea
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2020-09-04 16:37:05 +0530

    string method

---
+++ javastring/src/charAtmethod.java
@@ -0,0 +1,10 @@
+//The charAt() method returns the characters at the specified index in a string.
+
+public class charAtmethod {
+    public static void main(String[] args)
+    {
+        String mystr= "HELLO";
+        char result= mystr.charAt(1);
+        System.out.println(result);
+    }
+}
