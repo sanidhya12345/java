faeafb5a98c0d00e92d4c094807c363b40b7d2a8
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2020-09-04 16:37:11 +0530

    string method

---
+++ javastring/src/compareto_method.java
@@ -0,0 +1,8 @@
+//The compareTo() method compares two strings lexicograpically.
+public class compareto_method {
+    public static void main(String[] args){
+        String mystr1= "hello";
+        String mystr2= "sanidhya";
+        System.out.println(mystr1.compareTo(mystr2));
+    }
+}
