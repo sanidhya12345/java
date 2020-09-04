a5967544d65ad4e8c8c61d805d2c438169474ad8
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2020-09-04 15:06:49 +0530

    star pattern

---
+++ java_patterns/src/star_pattern3.java
@@ -0,0 +1,21 @@
+/*
+ *****
+ ****
+ ***
+ **
+ *
+ */
+
+public class star_pattern3 {
+    public static void main(String[] args)
+    {
+        for(int i=6;i>0;i--)
+        {
+            for(int j=1;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
