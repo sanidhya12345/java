ad63af62cc0a666acb92c52409c8a9dab7bbd8ab
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2020-09-04 14:57:47 +0530

    pattern

---
+++ java_patterns/src/number_pattern9.java
@@ -0,0 +1,21 @@
+/*
+12345
+1234
+123
+12
+1
+*/
+
+public class number_pattern9 {
+    public static void main(String[] args)
+    {
+        for(int i=5;i>0;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println(" ");
+        }
+    }
+}
