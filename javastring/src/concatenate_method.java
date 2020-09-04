5964d27c894c5a57b11bfcae2bcb25d9f0292a0b
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2020-09-04 16:37:15 +0530

    string method

---
+++ javastring/src/concatenate_method.java
@@ -0,0 +1,8 @@
+// the concat() method appends(concatenate) a string to the end of the another string.
+public class concatenate_method {
+    public static void main(String[] args){
+        String str1="hello";
+        String str2="sanidhya";
+        System.out.println(str1.concat(str2));
+    }
+}
