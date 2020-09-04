353374d12ca76a6b737e9c0ef428011640c34788
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2020-09-04 14:57:17 +0530

    pattern

---
+++ java_patterns/src/number_pattern6.java
@@ -0,0 +1,22 @@
+/*
+1
+22
+333
+4444
+55555
+*/
+
+
+public class number_pattern6 {
+    public static void main(String[] args)
+    {
+        for(int i=1;i<6;i++)
+        {
+            for(int j=1;j<i+1;j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println(" ");
+        }
+    }
+}
